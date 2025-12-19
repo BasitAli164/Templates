c6f141da5053cd58a2ba4610f74a01b938aa4481
Basit Ali <opoali911@gmail.com>
2025-12-19 22:58:44 +0500

    fix the serach form input for mobile device

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
-let searchForm=document.querySelector('.search-form');
+let searchForm = document.querySelector(".search-form");
 
-document.querySelector('#search-btn').onclick=()=>{
-    searchForm.classList.toggle('active');
-}
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  userIcon.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
+let shoppingCart = document.querySelector(".shopping-cart");
 
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  userIcon.classList.remove("active");
+  navbar.classList.remove("active");
+};
 
-let shoppingCart=document.querySelector('.shopping-cart');
+let userIcon = document.querySelector(".login-form");
 
-document.querySelector('#cart-btn').onclick=()=>{
-    shoppingCart.classList.toggle('active');
-}
+document.querySelector("#user-btn").onclick = () => {
+  userIcon.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+let navbar = document.querySelector(".navbar");
 
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  userIcon.classList.remove("active");
+};
 
-let userIcon=document.querySelector('.login-form');
-
-document.querySelector("#user-btn").onclick=()=>{
-    userIcon.classList.toggle('active')
-}
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  userIcon.classList.remove("active");
+  navbar.classList.remove("active");
+};
