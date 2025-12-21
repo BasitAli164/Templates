6fdeb123b2baa0e4ef6d235265a7d2d1db8a59f5
Basit Ali <opoali911@gmail.com>
2025-12-21 23:44:40 +0500

    fix one issue

---
+++ Day6/script.js
@@ -0,0 +1,35 @@
+let story=document.querySelector('.story')
+let set=document.querySelector('#set-content')
+let clear=document.querySelector('#clear-content')
+
+
+
+set.addEventListener('click',()=>{
+    // story.innerHTML="<b>basit</b>"
+    story.textContent="my name is basit"
+})
+clear.addEventListener('click',()=>{
+    // story.innerHTML=""
+    story.textContent=""
+})
+
+
+
+let parent=document.querySelector(".parent")
+let addChild=document.querySelector("#addChild")
+
+
+addChild.addEventListener("click",()=>{
+        
+
+        if (parent.childNodes.length > 1) {
+        
+        return;
+    }
+
+        const child=document.createElement("a")
+        child.classList.add("child")
+        child.textContent="child"
+        parent.appendChild(child)
+
+    })
