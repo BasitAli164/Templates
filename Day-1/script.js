c4d8cf54f929ac4ce14abcb87a902cc3a00debee
Basit Ali <opoali911@gmail.com>
2025-12-21 13:24:40 +0500

    working in new concept

---
+++ Day-1/script.js
@@ -0,0 +1,25 @@
+let images=document.querySelectorAll('.images')
+images.forEach(img => {
+    img.addEventListener('click',()=>{
+        removeClassList()
+        img.classList.add('active')
+    })
+    
+});
+
+
+function removeClassList(){
+    images.forEach(img=>{
+        img.classList.remove("active")
+    })
+}
+
+
+
+
+let btn=document.querySelector('.btn')
+
+btn.addEventListener('click',()=>{
+    btn.classList.toggle('.active')
+    console.log("object")
+})
--- Grocery Template/js/script.js
+++ Grocery Template/js/script.js
@@ -41,22 +41,3 @@ window.onscroll = () => {
 };
 
 
- var swiper = new Swiper(".product-slider", {
-      loop:true,
-      spaceBetween: 20,
-      autoplay:{
-        delay:7500,
-        disableOnInteraction:false,
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1020: {
-          slidesPerView: 3,
-        },
-      },
-    });
