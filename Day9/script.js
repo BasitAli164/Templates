bb571d546bc23d3e9c9886cf3951d3c56cc33b10
Basit Ali <opoali911@gmail.com>
2025-12-22 21:33:52 +0500

    working in dom

---
+++ Day9/script.js
@@ -0,0 +1,29 @@
+const myBody=document.querySelector("body");
+
+
+const table=document.createElement("table")
+const tablebody=document.createElement("tbody")
+
+
+for(let i=1;i<4;i++){
+    const tr=document.createElement('tr')
+    for (let j=1;j<5;j++){
+        const td=document.createElement('td')
+        const currentText=document.createTextNode(`Row ${i} and Col ${j}`)
+        td.appendChild(currentText)
+        tr.appendChild(td)
+    }
+
+
+    tablebody.appendChild(tr)
+
+    
+}
+
+
+table.appendChild(tablebody)
+
+myBody.appendChild(table)
+
+
+table.setAttribute("border",'2')
