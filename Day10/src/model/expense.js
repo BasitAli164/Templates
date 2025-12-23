e5ee16f51beed5d99cfa2233ce3261efa79cf5e3
Basit Ali <opoali911@gmail.com>
2025-12-23 22:10:10 +0500

    create models of user and expense

---
+++ Day10/src/model/expense.js
@@ -0,0 +1,24 @@
+class Expense{
+    constructor(paidBy,ammount,description="No Description"){
+        if(!paidBy || typeof paidBy!=="string"){
+            throw new Error ("PaidBy must be a non-empty string")
+        }
+
+        if(!ammount || typeof ammount!=="number" || ammount<=0){
+            throw new Error ("Ammount must be a positive integer")
+        }
+
+
+        this.paidBy=paidBy.trim();
+        this.ammount=parseFloat(ammount.toFixed(2));
+        this.description=description;
+        this.timeStamp=new Date().toISOString();
+        this.id=generateId();
+
+    }
+    
+    generateId(){
+        return crypto.randomUUID()
+    }
+
+}
