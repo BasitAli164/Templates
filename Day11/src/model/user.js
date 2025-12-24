a6c624538d071cd9bb609fcd4e430ffbd5c12160
Basit Ali <opoali911@gmail.com>
2025-12-24 16:42:29 +0500

    working in another project

---
+++ Day11/src/model/user.js
@@ -0,0 +1,19 @@
+export class User{
+    constructor(userName,userAge,userBio){
+        if(!userName || typeof userName!=="string"){
+            throw new Error(`User name must be a string`)
+        }
+        if(!userAge || typeof userAge!=="number" || userAge<=0){
+            throw new Error("User Age must be number and greater than 0")
+        }
+        
+        this.userName=userName.trim();
+        this.userAge=userAge;
+        this.userBio=userBio.trim();
+        this.id=this.generatedId();
+    }
+
+    generatedId(){
+        return crypto.randomUUID();
+    }
+}
