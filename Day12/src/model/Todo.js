2b836e9805ecd16baf11ab713e6f6190b6a946f3
Basit Ali <opoali911@gmail.com>
2025-12-25 15:34:45 +0500

    template is ready now start to work in logic part

--- Day12/src/model/Todo.js
+++ Day12/src/model/Todo.js
@@ -1,5 +1,12 @@
 export class Todo{
     constructor(todoTitle,todoDescription,todoStatus){
-        
+        this.todoTitle=this.todoTitle;
+        this.todoDescription=this.todoDescription;
+        this.todoStatus=this.todoStatus;
+        this.id=this.generateId()        
+    }
+
+    generateId(){
+        return crypto.randomUUID()
     }
 }
