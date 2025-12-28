168680a8a63e33b4bd848ec5e1d7641b5d713a34
Basit Ali <opoali911@gmail.com>
2025-12-28 14:20:30 +0500

    solving many error

--- Day17/script.js
+++ Day17/script.js
@@ -3,44 +3,47 @@
 let task = []; // create data structure to store task
 
 let newTask = document.getElementById("newTask");
+let taskList = document.getElementById("tasklist");
+const taskInput = document.getElementById("taskInput");
+
+console.log(taskList)
 
 function addTask() {
-  const taskInput = document.getElementById("taskInput");
   const text = taskInput.value.trim();
 
   if (!text) {
     throw new Error("There is not task you write ");
   }
-  const id = crypto.randomUUID();
+  let id = crypto.randomUUID();
   task.push({ id: id, text: text, complete: false });
   updateTaskList();
 }
 
 function updateTaskList() {
-  const taskList = document.getElementById("taskList");
   taskList.innerHTML = "";
+
   task.forEach((task) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-        <div class="taskItem ${task.complete?'completed':''}">
+        <div class="taskItem ${task.complete ? "completed" : ""}">
          <div class="task">
-         <input type="checkbox" class="checkBox" ${task.complete?"checked":""}/>
-         <p>Finish this project</p>
+         <input type="checkbox" class="checkBox" ${
+           task.complete ? "checked" : ""
+         }/>
+         <p>${task.text}</p>
          </div>
 
          <div class="icons">
-         <button>Edit</button>
-         <button>Delete</button>
+         <button onClick="editTask(${task.id})">Edit</button>
+         <button onClick="deleteTask(${task.id})">Delete</button>
          
          
-         </div>
-        
-        
+         </div>        
         </div>
 
         `;
-    listItem.addEventListener("change", () => toggleTaskComplete(task.id));
     taskList.appendChild(listItem);
+    listItem.addEventListener("change", () => toggleTaskComplete(task.id));
   });
 }
 
@@ -48,3 +51,13 @@ newTask.addEventListener("submit", (e) => {
   e.preventDefault();
   addTask();
 });
+
+function deleteTask(id) {
+  console.log(id);
+}
+function editTask() {
+  console.log(id);
+}
+function toggleTaskComplete(id) {
+  console.log(id);
+}
