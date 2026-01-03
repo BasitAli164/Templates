039da96c0a724fb739cc93cca8708b1b498c62fe
Basit Ali <opoali911@gmail.com>
2026-01-03 22:49:02 +0500

    facing error

--- Day22/script.js
+++ Day22/script.js
@@ -7,12 +7,44 @@ window.addEventListener("DOMContentLoaded", () => {
   let resultArea = document.getElementById("result");
 
   // create Data structure for saving data
-  let todos = JSON.parse(localStorage.getItem("todo")) || [];
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  console.log(todos);
 
   // create function
+  function saveToLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  function addTodoFun(e) {
+    try {
+      e.preventDefault();
+      const title = titleField.value.trim();
+      const description = descriptionField.value.trim();
+      console.log(description);
+      if (!title) {
+        throw new Error("Title is mendatory");
+      }
+      if (!description) {
+        throw new Error("Description is mendatory");
+      }
+
+      const newData = {
+        id: Date.now(),
+        createAt: new Date(),
+        title: title,
+        description: description,
+        complete: false,
+      };
+
+      todos.push(newData);
+      rendering();
+    } catch (error) {
+      console.error("Error facing during adding the todo", error);
+    }
+  }
 
   function rendering() {
-    const fragment = document.createDocumentFragment();
+    let fragment = document.createDocumentFragment();
     if (todos.length === 0) {
       const li = document.createElement("li");
       li.className = "emptyDataList";
@@ -20,18 +52,36 @@ window.addEventListener("DOMContentLoaded", () => {
       fragment.appendChild(li);
     } else {
       todos.forEach((todo) => {
-        const li = createElement(todo);
+        const li = createElements(todo);
         fragment.appendChild(li);
       });
     }
 
     resultArea.innerHTML = "";
-    resultArea.appendChild(fragment)
+    resultArea.appendChild(fragment);
+    saveToLocalStorage();
+    bindsAllEvent();
   }
 
-  function createElement(todo){
-    
+  function createElements(todo) {
+    const li = document.createElement("li");
+    li.className = "values";
+    li.dataset.id = todos.id;
+    li.innerHTML = `
+    <div class="box">
+    <div class"boxInside">
+    <input type="checkbox" class="checkBox" ${todo.complete ? "checked" : ""}/>
+    <bold>${todo.titel}</bold>    
+    </div>
+    <p>${todo.description}</p>
+    </div>     
+    `;
+  }
 
+  function bindsAllEvent() {
+    document
+      .querySelector("#addTodoForm")
+      .addEventListener("submit", addTodoFun);
   }
 
   rendering();
