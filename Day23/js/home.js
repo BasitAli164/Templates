ca664211a33137b70c1b8f3fcceef7e0237c7cd0
Basit Ali <opoali911@gmail.com>
2026-01-06 12:09:06 +0500

    working in project and eplore manything of location object in js

--- Day23/js/home.js
+++ Day23/js/home.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   // ========== Logout logic or code ===========
   const logOutBtn = document.getElementById("logoutBtn");
+  const bookingBtn=document.getElementById("bookTableBtn")
   const userData = JSON.parse(localStorage.getItem("userData"));
 
   if (!userData) {
@@ -10,7 +11,14 @@ window.addEventListener("DOMContentLoaded", () => {
     localStorage.removeItem("userData");
     window.location.replace("../index.html");
   });
-  
 
   // ========== Logout logic or code ===========
+
+  // ========== Booking Btn logic or code ===========
+  bookingBtn.addEventListener("click",()=>{
+    window.location.assign("../pages/bookingform.html")
+  })
+
+
+
 });
